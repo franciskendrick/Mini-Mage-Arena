4d9ca8d7d69e55e37606e17e76f16138bb2dcba7
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-02-21 18:34:35 +0800

    added Palette Swap in Functions

--- functions.py
+++ functions.py
@@ -153,6 +153,26 @@ def separate_sets_from_yaxis(set_img, separator_color):
     return separated_sets
 
 
+# Palette Swap
+def color_swap(img, old_color, new_color):
+    handle_img = pygame.Surface(img.get_size())
+    handle_img.fill(new_color)
+    img.set_colorkey(old_color)
+    handle_img.blit(img, (0, 0))
+
+    return handle_img
+
+
+def palette_swap(img, palette):
+    for old_color in palette:
+        new_color = palette[old_color]
+        img = color_swap(img, old_color, new_color)
+    img.set_colorkey((0, 0, 0))
+
+    return img
+
+
+
 # Collisions
 def rect_edge_collision(rect):
     left = window.arena_rect.left < rect.left
