2957a71ddc824119c0a5f9b1d087564f9a64bede
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-02-25 17:29:30 +0800

    added Mana Crystal Rectangle

--- supports.py
+++ supports.py
@@ -9,8 +9,9 @@ path = os.path.dirname(os.path.realpath(__file__))
 
 class ManaCrystal:
     # Initialize -------------------------------------------------- #
-    def __init__(self):
+    def __init__(self, center_pos):
         self.init_images()
+        self.init_rect(center_pos)
 
     def init_images(self):
         spriteset = pygame.image.load(
@@ -19,9 +20,14 @@ class ManaCrystal:
             spriteset, (255, 0, 0))
         order = ["small", "medium", "large"]
         self.type = 0
-        
+
         self.images = mana_spritesets[order[self.type]]
         self.idx = 0
+
+    def init_rect(self, center_pos):
+        img_rect = self.images[self.idx].get_rect()
+        self.rect = pygame.Rect(0, 0, *img_rect.size)
+        self.rect.center = center_pos
 
     # Draw -------------------------------------------------------- #
     def draw(self, display):
