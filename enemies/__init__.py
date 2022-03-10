55358812dc86331634d83e746f6da4dd5be6dc37
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-03-10 17:39:47 +0800

    added Boomshroom Module

--- enemies/__init__.py
+++ enemies/__init__.py
@@ -2,3 +2,4 @@ from .slime import Slime
 from .dark_mage import DarkMage
 from .mushroom import Mushroom
 from .fireshroom import Fireshroom
+from .boomshroom import Boomshroom
---
+++ enemies/boomshroom.py
@@ -0,0 +1,108 @@
+from functions import clip_set_to_list_on_xaxis, palette_swap
+from supports import ManaCrystal
+import pygame
+import time
+import os
+
+path = os.path.dirname(os.path.realpath("__main__"))
+
+
+class Boomshroom:
+    # Initialize -------------------------------------------------- #
+    def __init__(self):
+        self.init_images()
+        self.init_rect()
+        self.init_direction()
+        self.init_hit()
+        self.init_status()
+
+    def init_images(self):
+        # Spriteset
+        spriteset = pygame.image.load(
+            f"{path}/assets/sprites/boomshroom.png")
+        self.idx = 0
+
+        # Palettes
+        hit_palette = {
+            (9, 10, 20): (9, 10, 20),
+            (16, 20, 31): (168, 181, 178),
+            (21, 29, 40): (199, 207, 204),
+            (32, 46, 55): (168, 181, 178),
+            (57, 74, 80): (199, 207, 204),
+            (165, 48, 48): (235, 237, 233)}
+
+        # Images
+        self.images = {
+            "default": clip_set_to_list_on_xaxis(spriteset),
+            "hit": clip_set_to_list_on_xaxis(
+                palette_swap(spriteset.convert(), hit_palette))
+        }
+        self.image_used = "default"
+
+    def init_rect(self):
+        size = self.images[self.image_used][self.idx].get_rect().size
+        self.rect = pygame.Rect(100, 100, *size)
+
+    def init_direction(self):
+        self.direction = None
+
+    def init_hit(self):
+        self.last_hit = time.perf_counter()
+        self.hit_time = 300  # milliseconds
+
+    def init_status(self):
+        self.max_health = 12
+        self.health = 12
+        self.is_dead = False
+
+    # Draw -------------------------------------------------------- #
+    def draw(self, display):
+        # Reset
+        imgs = self.images[self.image_used]
+        if self.idx >= len(imgs) * 5:
+            self.idx = 0
+
+        # Direction
+        img = imgs[self.idx // 5]
+        if self.direction == "left":
+            img = pygame.transform.flip(img, True, False)
+
+        # Draw
+        display.blit(img, self.rect)
+
+        # Update
+        self.idx += 1
+
+    # Update ------------------------------------------------------ #
+    def update(self, player):
+        self.facing(player)
+        self.hit_timer()
+
+    # Direction
+    def facing(self, player):
+        if self.rect.centerx - player.rect.centerx > 0:  # left
+            self.direction = "left"
+        else:  # right
+            self.direction = "right"
+
+    # Hit 
+    def hit_timer(self):
+        if self.image_used == "hit":
+            dt = time.perf_counter() - self.last_hit
+            if dt * 1000 >= self.hit_time:
+                self.image_used = "default"
+
+    # Functions --------------------------------------------------- #
+    # Hit
+    def hit(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            self.is_dead = True
+
+        self.image_used = "hit"
+        self.last_hit = time.perf_counter()
+
+    # Mana
+    def mana_reward(self, mana_crystals):
+        for _ in range(round(self.max_health * 1.5)):
+            mana_crystals.append(ManaCrystal(self.rect.center))
