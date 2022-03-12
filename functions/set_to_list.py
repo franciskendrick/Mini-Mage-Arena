235fba104f8aca628893eda8aa666a4206101ff0
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-03-12 15:10:43 +0800

    added Set To List module in Functions package

---
+++ functions/set_to_list.py
@@ -0,0 +1,83 @@
+from .clip import clip
+
+
+def clip_set_to_list_on_xaxis(set):
+    images = []
+
+    # Loop Over every Pixel in Tileset
+    for x in range(set.get_width()):
+        pixel = set.get_at((x, 0))
+
+        # A Sprite/Tile is Found
+        if pixel == (255, 0, 255, 255):  # magenta
+            wd = 0
+            ht = 0
+
+            # Find the End of Sprites/Tiles in the X Coordinate
+            while True:
+                wd += 1
+                pixel = set.get_at((x + wd, 0))
+                if pixel == (0, 255, 255, 255):  # cyan
+                    break
+
+            # Find the End of Sprites/Tiles in the Y Coordinate
+            while True:
+                ht += 1
+                pixel = set.get_at((x, ht))
+                if pixel == (0, 255, 255, 255):  # cyan
+                    break
+
+            # Clip Image
+            img = clip(
+                set,
+                (x + 1, 1),
+                (wd - 1, ht - 1))
+
+            # Append
+            images.append(img)
+
+    # Unpack Images if Less Than One
+    [images] = [images] if len(images) > 1 else images
+
+    return images
+
+
+def clip_set_to_list_on_yaxis(set):
+    images = []
+
+    # Loop Over every Pixel in Tileset
+    for y in range(set.get_height()):
+        pixel = set.get_at((0, y))
+
+        # A Sprite/Tile is Found
+        if pixel == (255, 0, 255, 255):  # magenta
+            wd = 0
+            ht = 0
+
+            # Find the End of Sprites/Tiles in the X Coordinate
+            while True:
+                wd += 1
+                pixel = set.get_at((wd, y))
+                if pixel == (0, 255, 255, 255):  # cyan
+                    break
+
+            # Find the End of Sprites/Tiles in the Y Coordinate
+            while True:
+                ht += 1
+                pixel = set.get_at((0, y + ht))
+                if pixel == (0, 255, 255, 255):  # cyan
+                    break
+
+            # Clip Image
+            img = clip(
+                set,
+                (1, y + 1),
+                (wd - 1, ht - 1))
+
+            # Append
+            images.append(img)
+
+    # Unpack Images if Less Than One
+    [images] = [images] if len(images) > 1 else images
+
+    return images
