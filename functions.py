fc373e5b30da8799627281814e9e0d446f785fa4
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-03-12 15:26:59 +0800

    added Color Swap module in Functions package

--- functions.py
+++ functions.py
@@ -6,25 +6,6 @@ pygame.init()
 path = os.path.dirname(os.path.realpath("main.py"))
 
 
-# Palette Swap
-def color_swap(img, old_color, new_color):
-    handle_img = pygame.Surface(img.get_size())
-    handle_img.fill(new_color)
-    img.set_colorkey(old_color)
-    handle_img.blit(img, (0, 0))
-
-    return handle_img
-
-
-def palette_swap(img, palette):
-    for old_color in palette:
-        new_color = palette[old_color]
-        img = color_swap(img, old_color, new_color)
-    img.set_colorkey((0, 0, 0))
-
-    return img
-
-
 # Collisions
 def rect_edge_collision(rect):
     left = window.arena_rect.left < rect.left
