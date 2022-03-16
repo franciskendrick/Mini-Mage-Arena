98b6eb6dd520cd51236311cd7f4adb196c3d92d3
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-03-16 15:53:36 +0800

    added Normal Knight and its Images

---
+++ enemies/normal_knight.py
@@ -0,0 +1,24 @@
+import pygame
+import os
+
+pygame.init()
+path = os.path.dirname(os.path.realpath(__file__))
+
+
+class NormalKnight:
+    # Initialize -------------------------------------------------- #
+    def __init__(self):
+        pass
+
+    def init_images(self):
+        pass
+
+    # Draw -------------------------------------------------------- #
+    def draw(self, display):
+        pass
+
+    # Update ------------------------------------------------------ #
+    def update(self):
+        pass
+
+    # Functions --------------------------------------------------- #
