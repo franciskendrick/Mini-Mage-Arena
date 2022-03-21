11ffff225666b4a9e1f8f46d03f3ef57aa8b6da0
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-03-21 17:51:03 +0800

    added Background's top edge with its corners

--- windows/game/background.py
+++ windows/game/background.py
@@ -1,4 +1,4 @@
-from functions import clip_set_to_list_on_xaxis
+from functions import clip_set_to_list_on_xaxis, separate_sets_from_yaxis
 from windows import window
 import pygame
 import os
@@ -18,20 +18,16 @@ class Background:
         
         # Images
         self.images = {}
-        y_values = {
-            "black": 0,
-            "top": 18,
-            "bottom": 32,
-            "sides": 46,
-            "wall": 64,
-            "floor": 82}
-        for (name, y) in y_values.items():
-            images = clip_set_to_list_on_xaxis(spriteset, y)
-            self.images[name] = images
-        
+        order = ["black", "top", "bottom", "sides", "wall", "floor"]
+        separated_sets = separate_sets_from_yaxis(spriteset, (255, 0, 0))
+        for name, separated_set in zip(order, separated_sets):
+            image = clip_set_to_list_on_xaxis(separated_set)
+            self.images[name] = image
+
     # Draw -------------------------------------------------------- #
     def draw(self, display):
         self.draw_statusbar(display)
+        self.draw_arena_topedge(display)
 
     def draw_statusbar(self, display):
         image = self.images["black"]
@@ -42,8 +38,30 @@ class Background:
             for _ in range(window.rect.width // wd):
                 display.blit(image, (x, y))
                 x += wd
-            y += 16
+            y += ht
             x = 0
+
+    def draw_arena_topedge(self, display):
+        images = self.images["top"]
+        wd, _ = images[0].get_rect().size
+        x = 0
+        y = 32
+        
+        # TopLeft Corner
+        display.blit(images[0], (x, y))
+        x += wd
+
+        # First Top Edge
+        display.blit(images[1], (x, y))
+        x += wd
+
+        # The Rest of Top Edge
+        for _ in range((window.rect.width // wd) - 3):
+            display.blit(images[2], (x, y))
+            x += wd
+
+        # TopRight Corner
+        display.blit(images[3], (x, y))
 
 
 background = Background()
