e4c001fb6438a11ddc62c71dba05f7ddaa1f051d
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-03-27 13:08:34 +0800

    added Stamina Potion's images and rectangle
    - added Stamina Potion's images
    - added Stamina Potion's rectangle

--- supports/stamina_potion.py
+++ supports/stamina_potion.py
@@ -1,12 +1,28 @@
+from functions import clip_set_to_list_on_xaxis
 import pygame
+import os
 
 pygame.init()
+path = os.path.dirname(os.path.realpath(__file__))
 
 
 class StaminaPotion:
     # Initialize -------------------------------------------------- #
     def __init__(self):
-        pass
+        self.init_images()
+        self.init_rect()
+
+    def init_images(self):
+        spriteset = pygame.image.load(
+            f"{path}/assets/stamina_potion.png")
+
+        # Images
+        self.images = clip_set_to_list_on_xaxis(spriteset)
+        self.idx = 0
+    
+    def init_rect(self):
+        img_rect = self.images[self.idx].get_rect()
+        self.rect = pygame.Rect(200, 200, *img_rect.size)
 
     # Draw -------------------------------------------------------- #
     def draw(self, display):
