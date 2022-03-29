3595debc13ca6f24d7524188c94484c58a875f1b
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-03-29 12:49:35 +0800

    added Snowbilize's color and movement

--- spells/player/snowbilize.py
+++ spells/player/snowbilize.py
@@ -1,4 +1,7 @@
+from functions import Circle
+from windows import window
 import pygame
+import math
 
 pygame.init()
 
@@ -6,7 +9,24 @@ pygame.init()
 class Snowbilize:
     # Initialize -------------------------------------------------- #
     def __init__(self, origin, target):
-        pass
+        self.circle = Circle(origin, 4)
+        self.init_color()
+        self.init_movement(target)
+        self.damage = 3
+        self.mana_cost = 6
+        self.collided = False
+
+    def init_color(self):
+        self.color = (164, 221, 219)
+
+    def init_movement(self, target):
+        target_x, target_y = target
+        self.speed = 6
+        angle = math.atan2(
+            target_y - self.circle.center[1] * window.enlarge, 
+            target_x - self.circle.center[0] * window.enlarge)
+        self.x_vel = math.cos(angle)
+        self.y_vel = math.sin(angle)
 
     # Draw -------------------------------------------------------- #
     def draw(self, display):
