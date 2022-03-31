d323c2da6641e26dc2dc04a3c83c5b6a7c124d9e
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-03-31 13:32:17 +0800

    added Water Drop's movement

--- spells/player/water_drop.py
+++ spells/player/water_drop.py
@@ -40,4 +40,10 @@ class WaterDrop:
 
     # Update ------------------------------------------------------ #
     def update(self, enemies):
-        pass
+        if not self.collided:
+            self.movement()
+
+    # Movement
+    def movement(self):
+        self.circle.center[0] += (self.x_vel * self.speed)
+        self.circle.center[1] += (self.y_vel * self.speed)
